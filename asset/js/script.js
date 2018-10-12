19b2ea92887d9dcab8ae3d760fc5cca1b1ad6069
dannielarenderos <00087717@uca.edu.sv>
2018-10-12 08:43:31 -0600

    Segundo Comment

---
+++ asset/js/script.js
@@ -0,0 +1,36 @@
+var container = document.getElementById("c1");
+
+container.innerText = "¡Hola Mundo!";
+
+
+container.style.backgroundColor = "#000000";
+
+container.style.color =  "#1cb723";
+
+container.style.width = "200px";
+container.style.height = "200px";
+
+
+container.innerHTML = "<input type = text placeholder = 'Escriba su texto'>";
+
+
+var containers = document.getElementsByClassName("c2");
+
+for (let i= 0; i<containers.length;i++){
+    containers[i].style.backgroundColor = "#009999";
+    containers[i].innerHTML ="<input type = text placeholder = 'escriba su texto'>";
+}
+
+var boton = document.getElementById("btn-click");
+
+boton.onclick = function(evt){alert("Hola mundo")};
+
+var btnCopy = document.getElementById("btnCopy");
+
+
+btnCopy.onclick = function(evt) 
+{ 
+let msj = document.getElementById('textMsj').value;
+document.getElementById('showMsj').innerText = msj;
+}
+
